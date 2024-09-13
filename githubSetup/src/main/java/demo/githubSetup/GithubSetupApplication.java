562eb15c3b6d62914928255ecba3201d49ddb91f
Robin Ruud Waale <97589948+robinruudW@users.noreply.github.com>
2024-09-13 09:43:06 +0200

    Add files via upload

---
+++ githubSetup/src/main/java/demo/githubSetup/GithubSetupApplication.java
@@ -0,0 +1,13 @@
+package demo.githubSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSetupApplication.class, args);
+	}
+
+}
---
+++ githubSetup/src/test/java/demo/githubSetup/GithubSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.githubSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
